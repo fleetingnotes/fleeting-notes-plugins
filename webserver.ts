cdb020bb97ddb1800a06b629cb5dd1b944eb558f
Matthew Wong <matthewwong525@gmail.com>
2023-05-19 11:27:49 -0400

    add dynamic route generation by creating dynamic manifest

--- webserver.ts
+++ webserver.ts
@@ -1,35 +1,12 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import importModule from './routes-manifest.ts'
 
-const getRoutes = async (dir: string): Promise<Map<string, Function>> => {
-  const entries = Deno.readDir(dir);
-  const routes = new Map();
-
-  for await (const entry of entries) {
-    const path = `${dir}/${entry.name}`;
-
-    if (entry.isFile && path.endsWith('.ts')) {
-      const module = await import(path);
-      const pathname = dir.replace('.', '');
-      routes.set(pathname, module.default);
-    } else if (entry.isDirectory) {
-      const nestedRoutes = await getRoutes(path);
-      nestedRoutes.forEach((value, key) => {
-        routes.set(key, value);
-      });
-    }
-  }
-
-  return routes;
-}
-
-const routes = await getRoutes('./plugins');
 
 function handler(_req: Request): Response {
   const pathname = new URL(_req.url).pathname;
-  const handler = routes.get(pathname);
-
-  if (handler) {
-    return handler();
+  const module = importModule(`.${pathname.replace(/\/$/, '')}/index.ts`);
+  if (module) {
+    return module.default(_req);
   }
   return new Response(_req.headers.get('host') ?? 'unknown');
 }
