08ceaa3b9631a41cd9624c778b4da5765b1a26fe
Matthew Wong <matthewwong525@gmail.com>
2023-05-28 15:07:13 -0400

    use metadata as a prompt

--- plugins/official/chat-gpt/index.ts
+++ plugins/official/chat-gpt/index.ts
@@ -5,17 +5,21 @@ export default async (request: Request) : Promise<Response> => {
   const json = await request.json()
   const apiKey = Deno.env.get("OPEN_AI_KEY")!;
   const openAI = new OpenAI(apiKey);
+  const metadata: string = json['metadata'];
   const note = json['note'];
   if (!note) {
     return new Response("Couldn't find note in request", { status: 400 });
   }
-  const noteContent = note['content'] || '';
+  const noteContent: string = note['content'] || '';
   try {
+    const messages: {role: "system" | "user", content: string}[] = (metadata) ? [
+      {"role": "system", "content": metadata}
+    ] : [];
+    messages.push({"role": "user", "content": noteContent})
+    
     const chatCompletion = await openAI.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: [
-        {"role": "user", "content": noteContent}
-      ]
+      messages: messages,
     });
     const message : string | null | undefined = chatCompletion.choices?.[0]?.message.content;
     if (!message) return new Response('No message from OpenAI', { status: 400});
