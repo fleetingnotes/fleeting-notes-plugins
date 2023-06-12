cda1e250fdaa64821749101394da763581ffba7c
Gonzalo Velasco <vela8624@gmail.com>
2023-06-12 09:12:21 -0300

    add validations and format with deno lint (#15)
    * add validations and format with deno lint
    
    * move validations to utils

--- plugins/official/summarize-source/index.ts
+++ plugins/official/summarize-source/index.ts
@@ -1,12 +1,12 @@
-import { chatGpt3 } from "../../../utils.ts";
+import { chatGpt3, validateURI } from "../../../utils.ts";
 import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
-import { Readability } from "https://esm.sh/@mozilla/readability@0.4.4"
+import { Readability } from "https://esm.sh/@mozilla/readability@0.4.4";
 import "https://deno.land/std@0.188.0/dotenv/load.ts";
 
 function extractContent(html: string) {
   const doc = new DOMParser().parseFromString(html, "text/html");
   const article = new Readability(doc).parse();
-  return article?.textContent || '';
+  return article?.textContent || "";
 }
 
 async function fetchWebPage(url: string) {
@@ -15,39 +15,43 @@ async function fetchWebPage(url: string) {
   return html;
 }
 
-
-async function summarizeText(text: string, systemPrompt:string) {
+async function summarizeText(text: string, systemPrompt: string) {
   if (text.length < 12000) {
     return chatGpt3(text, systemPrompt);
   } else if (text.length > 100000) {
-    throw new Error('Source is too long (> 100k characters)');
+    throw new Error("Source is too long (> 100k characters)");
   }
-  const chunks = text.match(/[\s\S]{1,12000}/g) || []; 
+  const chunks = text.match(/[\s\S]{1,12000}/g) || [];
   const responses = await Promise.all(chunks.map((c: string) => {
-    return chatGpt3('Summarize:\n' +  c);
+    return chatGpt3("Summarize:\n" + c);
   }));
-  return chatGpt3(responses.join('\n'), systemPrompt);
+  return chatGpt3(responses.join("\n"), systemPrompt);
 }
 
-
-export default async (request: Request) : Promise<Response> => {
-  const json = await request.json()
-  const source: string = json['note']?.source || '';
-  const metadata: string = json['metadata'];
-  if (!source) {
-    return new Response("Source field empty", { status: 400 });
+export default async (request: Request): Promise<Response> => {
+  const json = await request.json();
+  const source: string = json["note"]?.source || "";
+  const metadata: string = json["metadata"];
+  const { passes, error } = validateURI(source);
+  if (!passes) {
+    return new Response(error, { status: 400 });
   }
 
   const html = await fetchWebPage(source);
   const text = extractContent(html);
-  if (text.length == 0) return new Response("Failed to extract text from source", { status: 400 })
-  const systemPrompt = metadata || "Summarize the following text with the most unique and helpful points, into a bullet list of key points and takeaways";
+  if (text.length == 0) {
+    return new Response("Failed to extract text from source", { status: 400 });
+  }
+  const systemPrompt = metadata ||
+    "Summarize the following text with the most unique and helpful points, into a bullet list of key points and takeaways";
 
   try {
     const message = await summarizeText(text, systemPrompt);
-    if (!message) return new Response('No message from OpenAI', { status: 400 });
+    if (!message) {
+      return new Response("No message from OpenAI", { status: 400 });
+    }
     return new Response(message);
   } catch (e) {
-    return new Response(e, {status: 400});
+    return new Response(e, { status: 400 });
   }
-}
+};
--- utils.ts
+++ utils.ts
@@ -1,17 +1,42 @@
 import { OpenAI } from "https://deno.land/x/openai@1.3.4/mod.ts";
 
-export async function chatGpt3(text: string, systemPrompt?: string) : Promise<string> {
-  const apiKey = Deno.env.get("OPEN_AI_KEY")!;
-  const openAI = new OpenAI(apiKey);
-  const messages: {role: "system" | "user", content: string}[] = (systemPrompt) ? [
-    {"role": "system", "content": systemPrompt}
-  ] : [];
-  messages.push({"role": "user", "content": text})
-  
+export async function chatGpt3(
+  text: string,
+  systemPrompt?: string,
+): Promise<string> {
+  const API_KEY = Deno.env.get("OPEN_AI_KEY")!;
+  console.log("API_KEY", API_KEY);
+  const openAI = new OpenAI(API_KEY);
+  const messages: { role: "system" | "user"; content: string }[] =
+    (systemPrompt)
+      ? [
+        { "role": "system", "content": systemPrompt },
+      ]
+      : [];
+  messages.push({ "role": "user", "content": text });
+
   const chatCompletion = await openAI.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: messages,
   });
-  const message : string | undefined = chatCompletion.choices?.[0]?.message.content;
-  return message || '';
+  const message: string | undefined =
+    chatCompletion.choices?.[0]?.message.content;
+  return message || "";
+}
+
+type ErrorMessage = {
+  passes: boolean;
+  error: string | null;
+};
+export function validateURI(uri: string): ErrorMessage {
+  if (!uri) {
+    return { passes: false, error: "Source field empty" };
+  }
+  try {
+    new URL(uri);
+    return { passes: true, error: null };
+  } catch (e) {
+    console.log(e);
+    return { passes: false, error: "Source field is not a valid Url" };
+  }
 }
